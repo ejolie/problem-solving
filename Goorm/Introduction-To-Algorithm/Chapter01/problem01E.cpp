ead70ea81ae9614435fa2c7551a7fc4f711b4e7c
Eunjeong Park <ejisvip@gmail.com>
2018-09-01 02:27:46 +0900

    problem01E: 합 구하기2

---
+++ Goorm/Introduction-To-Algorithm/Chapter01/problem01E.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+void solve(int data[], int n, int p, int q) {
+  int count = 0;
+  int sum = 0;
+
+  for (int i = 0 ; i < n ; i++) {
+    if (data[i] <= p) {
+      count++;
+      sum += data[i];
+    }
+  }
+
+  printf("%d %d\n", count, sum);
+  sum < q ? printf("YES") : printf("NO");
+
+}
+
+int main() {
+  int n, p, q;
+  int * data[];
+
+  scanf("%d %d %d", &n, &p, &q);
+  data = new int[n];
+
+  for (int i = 0 ; i < n ; i++) {
+    scanf("%d", data[i]);
+  }
+
+  solve(data, n, p, q);
+
+  delete[] data;
+  return 0;
+}
