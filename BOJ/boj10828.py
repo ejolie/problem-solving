3255495a34656679952472d49adc6e1db5e8d690
unknown <ejisvip@gmail.com>
2019-01-03 14:27:19 +0900

    boj10828: 스택

---
+++ BOJ/boj10828.py
@@ -0,0 +1,35 @@
+import sys
+
+class Stack():
+    def __init__(self):
+        self.myList = []
+
+    def empty(self):
+        return 1 if self.myList == [] else 0
+
+    def push(self, item):
+        self.myList.append(item)
+    
+    def pop(self):
+        return -1 if self.empty() else self.myList.pop() 
+
+    def size(self):
+        return len(self.myList)
+
+    def top(self):
+        return -1 if self.empty() else self.myList[-1]
+
+n = int(input())
+s = Stack()
+for i in range(n):
+    cmd = list(sys.stdin.readline().split())
+    if cmd[0] == 'push':
+        s.push(int(cmd[1]))
+    elif cmd[0] == 'pop':
+        print(s.pop())
+    elif cmd[0] == 'empty':
+        print(s.empty())
+    elif cmd[0] == 'size':
+        print(s.size())
+    elif cmd[0] == 'top':
+        print(s.top())
