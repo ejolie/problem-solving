088acf9edf8ebd6a363f6a3bedb18a25ae107b98
Eunjeong Park <ejisvip@gmail.com>
2019-01-05 22:21:00 +0900

    Add nested-lists.py

---
+++ Hackerrank/nested-lists.py
@@ -0,0 +1,6 @@
+import sys
+
+n = int(input())
+studentList = [[input(), float(input())] for _ in range(n)]
+secondMax = sorted(set([sc for nm, sc in studentList]))[1]
+print('\n'.join(sorted([nm for nm, sc in studentList if secondMax == sc])))
