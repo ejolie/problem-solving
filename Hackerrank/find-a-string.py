198bfabeb57cda987811604ef2d90df8be3b0396
Eunjeong Park <ejisvip@gmail.com>
2019-01-09 21:40:59 +0900

    Add find-a-string.py

--- Hackerrank/find-a-string.py
+++ Hackerrank/find-a-string.py
@@ -1,7 +1,7 @@
 def count_substring(string, sub_string):
-  return string.count(sub_string)
-
+  for i in range(len(s)):
+    
 if __name__ == '__main__':
-  string = input()
-  sub_string = input()
+  string = input().strip()
+  sub_string = input().strip()
   print(count_substring(string, sub_string))
