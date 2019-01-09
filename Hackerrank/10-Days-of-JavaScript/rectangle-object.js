c22ada4c62213b22d12c89ad03a8928f1836a3d8
Eunjeong Park <ejisvip@gmail.com>
2019-01-10 01:36:35 +0900

    Add rectangle-object.js

---
+++ Hackerrank/10-Days-of-JavaScript/rectangle-object.js
@@ -0,0 +1,18 @@
+function Rectangle(a, b) {
+  this.length = a;
+  this.width = b;
+  this.perimeter = 2*(a+b);
+  this.area = a*b;
+}
+
+/* ES6
+class Rectangle {
+  constructor(a, b) {
+    this.length = a;
+    this.width = b;
+    this.perimeter = 2*(a+b);
+    this.area = a*b;    
+  }
+}
+
+*/
