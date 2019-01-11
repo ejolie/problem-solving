f72f69f385c08447a825ff23becb9b7a1c1b8c4c
unknown <ejisvip@gmail.com>
2019-01-11 17:38:26 +0900

    Add diagonal-difference.py

---
+++ Hackerrank/Python/diagonal-difference.py
@@ -0,0 +1,13 @@
+# def diagonalDifference(arr):
+#     for i in range(len(arr)):
+        
+
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().rstrip().split())))
+
+print(arr)
+
+# result = diagonalDifference(arr)
+# print(result)
