941d9776527803d38efb66069baf45e6c4715502
Eunjeong Park <ejisvip@gmail.com>
2019-01-11 22:08:16 +0900

    Add switch.js

--- Hackerrank/10-Days-of-JavaScript/switch.js
+++ Hackerrank/10-Days-of-JavaScript/switch.js
@@ -1,6 +1,20 @@
 function getLetter(s) {
   let letter;
-
+  
+  switch (true) {
+    case 'aeiou'.includes(s[0]):
+      letter = 'A';
+      break;
+    case 'bcdfg'.includes(s[0]):
+      letter = 'B';
+      break;
+    case 'hjklm'.includes(s[0]):
+      letter = 'C';
+      break;
+    case 'npqrstvwxyz'.includes(s[0]):
+      letter = 'D';
+      break;
+  }
 
   return letter;
 }
