969deaee27c07731fdada6f1c68405692eaf22d0
unknown <ejisvip@gmail.com>
2019-01-15 17:23:47 +0900

    Add inheritance.js

---
+++ Hackerrank/10-Days-of-JavaScript/inheritance.js
@@ -0,0 +1,16 @@
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+
+Rectangle.prototype.area = function() {
+    return this.w * this.h;
+}
+
+class Square extends Rectangle {
+    constructor(s) {
+        super(s, s);
+    }
+}
