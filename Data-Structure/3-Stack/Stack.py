30ee0ca76619df7ab0c46a04a9c9a00a4b441241
unknown <ejisvip@gmail.com>
2019-02-25 17:15:51 +0900

    Modify Stack.py

--- Data-Structure/3-Stack/Stack.py
+++ Data-Structure/3-Stack/Stack.py
@@ -1,18 +1,18 @@
 class Stack:
     def __init__(self):
-        self.items = []
+        self.stack = []
 
     def push(self, item):
-        self.items.append(item)
+        self.stack.append(item)
 
     def pop(self):
-        return -1 if self.isEmpty() else self.items.pop()
+        return -1 if self.isEmpty() else self.stack.pop()
 
     def peek(self):
-        return -1 if self.isEmpty() else self.items[-1]
+        return -1 if self.isEmpty() else self.stack[-1]
 
     def isEmpty(self):
-        return self.items == []
+        return self.stack == []
 
     def size(self):
-        return len(self.items)
+        return len(self.stack)
