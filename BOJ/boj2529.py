1128a717376b31bd325d5c752a5edf330bca07d3
Eunjeong Park <ejisvip@gmail.com>
2019-03-12 09:12:56 +0900

    Create boj2529.py

---
+++ BOJ/boj2529.py
@@ -0,0 +1,16 @@
+'''
+boj2529. 부등호
+'''
+from sys import stdin
+def solve(k):
+    for i in range(K):
+        for j in range(10):
+
+K = int(stdin.readline().rstrip())
+symbols = list(stdin.readline().rstrip().split())
+check = [0] * 10
+nums = [0] * (K + 1)
+maxnum = 0
+solve(0)
+
+
