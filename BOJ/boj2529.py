095ba830964284a4e824bf94f414d62cfcc65694
ejolie <ejisvip@gmail.com>
2019-03-12 15:05:04 +0900

    Modify boj2529.py

--- BOJ/boj2529.py
+++ BOJ/boj2529.py
@@ -1,10 +1,19 @@
 '''
 boj2529. 부등호
+
+Unsolved
 '''
 from sys import stdin
 def solve(k):
-    for i in range(K):
+    if k == K+2:
+        return
+    for i in range(k, K):
         for j in range(10):
+            if not check[j]:
+                check[j] = 1
+                solve(k+1)
+                check[j] = 0
+
 
 K = int(stdin.readline().rstrip())
 symbols = list(stdin.readline().rstrip().split())
