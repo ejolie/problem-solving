2c8c82a18103732e3820a942c11d638308b6b6c9
Eunjeong Park <ejisvip@gmail.com>
2019-03-16 17:16:49 +0900

    swea5255: 타일 붙이기

--- SWEA/D3/swea5255.py
+++ SWEA/D3/swea5255.py
@@ -1,17 +1,12 @@
 '''
 swea5255. 타일 붙이기
+dp[n] = dp[n-1] + 2*dp[n-2] + dp[n-3]
 '''
-def solve(N):
-    memo = [0] * (N+1)
-    memo[0], memo[1], memo[2] = 0, 1, 3
-    for i in range(3, N+1):
-        if i & 1 == 0: # 짝수
-            memo[i] = 2 * memo[i-2] + memo[i-1] + (i // 3)
-        else:
-            memo[i] = memo[i-3] + memo[i-2] + memo[i-1] + (i // 4)
-    return memo[N]
-
+dp = [0] * 31
+dp[1], dp[2], dp[3] = 1, 3, 6
+for i in range(4, 31):
+    dp[i] = dp[i-1] + 2*dp[i-2] + dp[i-3] 
 tc = int(input())
 for t in range(1, tc+1):
     N = int(input())
-    print('#{} {}'.format(t, solve(N)))
+    print('#{} {}'.format(t, dp[N]))
