167d29b65840b8ae7de724f305d84c0fc11cb3d6
ejolie <ejisvip@gmail.com>
2019-03-18 16:23:15 +0900

    swea3750: Digit sum

--- SWEA/D3/swea3750.py
+++ SWEA/D3/swea3750.py
@@ -1,12 +1,15 @@
 '''
 swea3750. Digit sum (D3)
 '''
+res = []
 tc = int(input())
-for t in range(1, tc+1):
+for t in range(tc):
     num = input()
     while len(num) > 1:
         tmp = 0
         for n in num:
             tmp += int(n)
         num = str(tmp)
-    print('#{} {}'.format(t, num))
+    res.append(num)
+for t in range(tc):
+    print('#{} {}'.format(t+1, res[t]))
