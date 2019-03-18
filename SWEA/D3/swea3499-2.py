072ef2d1977c5152006542f92ffaf72e17901b5d
ejolie <ejisvip@gmail.com>
2019-03-18 16:18:53 +0900

    Rename swea3499.py

--- SWEA/D3/swea3499-2.py
+++ SWEA/D3/swea3499-2.py
@@ -1,16 +1,5 @@
 '''
 swea3499. 퍼펙트 셔플 (D3)
-
-m = n//2 = 2
-A B C D E
-0 1 2 3 4
-
-A B C / D E
-A, D, B, E, C
-
-ALAKIR ALEXSTRASZA DR-BOOM / LORD-JARAXXUS AVIANA
-ALAKIR LORD-JARAXXUS ALEXSTRASZA AVIANA DR-BOOM
-
 '''
 tc = int(input())
 for t in range(1, tc+1):
@@ -21,12 +10,13 @@ for t in range(1, tc+1):
     else:
         l, r = cards[:N//2], cards[N//2:]
     i, j = 0, 0
+    res = []
     print('#%d' % t, end=' ')
     while i < len(l) and j < len(r):
-        print(l[i], end=' ')
-        print(r[j], end=' ')
+        res.append(l[i])
+        res.append(r[j])
         i += 1; j += 1
     while i < len(l):
-        print(l[i], end=' ')
+        res.append(l[i])
         i += 1
-    print()
+    print(' '.join(res))
