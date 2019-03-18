62b8f0174ed2f47eebb752db937aa39efe57edd5
Eunjeong Park <ejisvip@gmail.com>
2019-03-18 23:17:59 +0900

    Modify swea2805.py

--- SWEA/D3/swea2805.py
+++ SWEA/D3/swea2805.py
@@ -12,14 +12,16 @@ a[6][3]
 '''
 tc = int(input())
 for t in range(1, tc+1):
-	N = int(input()) # 7
-	MAP = [[] * N for _ in range(N)]
+	N = int(input())
+	mat = [0] * N
 	for i in range(N):
-		MAP[i] = [int(n) for n in list(input())]
-	ans = 0; M = N//2 # 3
-	for i in range(M): # 0, 1, 2
-		ans += sum(MAP[i][M-i:M+i+1]) # 3, 2, 1 ~
-	ans += sum(MAP[M])
-	for i in range(M-1, -1, -1): # 2, 1, 0
-		ans += sum(MAP[N-1-i][M-i:M+i+1]) # 4, 5, 6 ~ 
+		mat[i] = list(map(int, list(input())))
+
+	ans = 0
+	M = N // 2
+	for i in range(M):
+		ans += sum(mat[i][M-i:M+i+1])
+	ans += sum(mat[M])
+	for i in range(M-1, -1, -1):
+		ans += sum(mat[N-1-i][M-i:M+i+1])
 	print('#%d %d' % (t, ans))
