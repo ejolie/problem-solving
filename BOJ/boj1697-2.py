ed0dd1bb6b92cb04b649ca4bc0fd7ebbc4afb44f
Eunjeong Park <ejisvip@gmail.com>
2019-03-25 15:28:39 +0900

    boj1697: 숨바꼭질

---
+++ BOJ/boj1697-2.py
@@ -0,0 +1,26 @@
+'''
+boj1697. 숨바꼭질
+'''
+from sys import stdin
+def bfs(A):
+	q = [A]
+	visited[A] = 1
+	while q != []:
+		x = q.pop(0)
+		if x == B:
+			return visited[x] - 1
+		if 0 <= x-1 < N-1 and not visited[x-1]:
+			visited[x-1] = visited[x] + 1
+			q.append(x-1)
+		if 0 <= x+1 < N-1 and not visited[x+1]:
+			visited[x+1] = visited[x] + 1
+			q.append(x+1)
+		if 0 <= 2*x < N-1 and not visited[2*x]:
+			visited[2*x] = visited[x] + 1
+			q.append(2*x)
+
+A, B = map(int, stdin.readline().rstrip().split())
+N = 1000001
+mat = [0] * N
+visited = [0] * N
+print('%d' % bfs(A))
