e5a573358a2e674e110a539ab66152bb130687c1
Eunjeong Park <ejisvip@gmail.com>
2019-03-26 23:21:15 +0900

    Modify 숨바꼭질 code

--- BOJ/boj1697-2.py
+++ BOJ/boj1697-2.py
@@ -9,17 +9,17 @@ def bfs(A):
 		x = q.pop(0)
 		if x == B:
 			return visited[x] - 1
-		if 0 <= x-1 < N-1 and not visited[x-1]:
+		if 0 <= x-1 < N and not visited[x-1]:
 			visited[x-1] = visited[x] + 1
 			q.append(x-1)
-		if 0 <= x+1 < N-1 and not visited[x+1]:
+		if 0 <= x+1 < N and not visited[x+1]:
 			visited[x+1] = visited[x] + 1
 			q.append(x+1)
-		if 0 <= 2*x < N-1 and not visited[2*x]:
+		if 0 <= 2*x < N and not visited[2*x]:
 			visited[2*x] = visited[x] + 1
 			q.append(2*x)
 
 A, B = map(int, stdin.readline().rstrip().split())
-N = 1000001
+N = 100001g
 visited = [0] * N
 print('%d' % bfs(A))
--- BOJ/boj1697.py
+++ BOJ/boj1697.py
@@ -3,23 +3,23 @@ boj1697. 숨바꼭질
 '''
 from sys import stdin
 def bfs(A):
+	visited[A] = 1
 	q = [(A, 0)]
 	while q != []:
 		x, cnt = q.pop(0)
 		if x == B:
 			return cnt
-		if 0 <= x-1 < N-1 and not visited[x-1]:
+		if 0 <= x-1 < N and not visited[x-1]:
 			visited[x-1] = 1
 			q.append((x-1, cnt+1))
-		if 0 <= x+1 < N-1 and not visited[x+1]:
+		if 0 <= x+1 < N and not visited[x+1]:
 			visited[x+1] = 1
 			q.append((x+1, cnt+1))
-		if 0 <= 2*x < N-1 and not visited[2*x]:
+		if 0 <= 2*x < N and not visited[2*x]:
 			visited[2*x] = 1
 			q.append((2*x, cnt+1))
 
 A, B = map(int, stdin.readline().rstrip().split())
-N = 1000001
-mat = [0] * N
+N = 100001
 visited = [0] * N
 print('%d' % bfs(A))
