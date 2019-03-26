eab8e8ed0ab54a69ab0b8f84e17fc94f56928196
Eunjeong Park <ejisvip@gmail.com>
2019-03-26 23:23:10 +0900

    Modify 숨바꼭질 3 code

--- BOJ/boj13549.py
+++ BOJ/boj13549.py
@@ -2,9 +2,10 @@
 boj13549. 숨바꼭질 3
 '''
 from sys import stdin
-def bfs(A, B):
+def bfs(v):
+	visited[A] = 1
 	q = [(A, 0)]
-	mincnt = 1000001
+	mincnt = N
 	while q != []:
 		x, cnt = q.pop(0)
 		if cnt >= mincnt:
@@ -14,18 +15,18 @@ def bfs(A, B):
 			if cnt < mincnt:
 				mincnt = cnt
 		else:
-			if 0 <= 2*x < N-1 and not visited[2*x]:
+			if 0 <= 2*x < N and not visited[2*x]:
 				visited[2*x] = 1
 				q.append((2*x, cnt))
-			if 0 <= x-1 < N-1 and not visited[x-1]:
+			if 0 <= x-1 < N and not visited[x-1]:
 				visited[x-1] = 1
 				q.append((x-1, cnt+1))
-			if 0 <= x+1 < N-1 and not visited[x+1]:
+			if 0 <= x+1 < N and not visited[x+1]:
 				visited[x+1] = 1
 				q.append((x+1, cnt+1))
 	print(mincnt)
 
 A, B = map(int, stdin.readline().rstrip().split())
-N = 1000001
+N = 100001
 visited = [0] * N
-bfs(A, B)
+bfs(A)
