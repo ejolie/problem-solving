0a79e05f180e30de54d141357ace8fd87dccc7ad
Eunjeong Park <ejisvip@gmail.com>
2019-03-27 21:16:37 +0900

    boj6603: 로또

--- BOJ/boj6603.py
+++ BOJ/boj6603.py
@@ -1,28 +1,24 @@
 '''
 boj6603. 로또
 Backtracking
-
-k = 8
-S = 1 2 3 5 8 13 21 34
 '''
 from sys import stdin
-def solve(idx, h):
-	if h == 6:
+def solve(v, cnt):
+	if cnt == 6:
 		for i in range(6):
 			print(res[i], end=' ')
 		print()
 		return
-
-	for i in range(idx, K):
-		res[h] = arr[i]
-		solve(i + 1, h + 1)
+	for i in range(v, K):
+		res[cnt] = arr[i]
+		solve(i + 1, cnt + 1)
 
 while True:
 	tmp = stdin.readline().rstrip().split()
 	if tmp == ['0']:
 		break
-	K = tmp[0]
+	K = int(tmp[0])
 	arr = tmp[1:]
 	res = [0] * 6
-	for s in range(0, K - 5):
-		solve(s, 0)
+	solve(0, 0)
+	print()
