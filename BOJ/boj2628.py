0ac1b78c1f26ef683920b2a80bbc44ac5afd72c1
Eunjeong Park <ejisvip@gmail.com>
2019-03-28 16:59:09 +0900

    boj2628: 종이자르기

--- BOJ/boj2628.py
+++ BOJ/boj2628.py
@@ -1,3 +1,25 @@
 '''
 boj2628. 종이자르기
 '''
+def find_max(arr, N):
+    maxleng = 0
+    s = 0
+    for i in range(1, N):
+        if arr[i]:
+            if i - s > maxleng:
+                maxleng = i - s
+            s = i
+    return maxleng
+
+W, H = map(int, input().split())
+N = int(input())
+garo = [0] * (W+1)
+sero = [0] * (H+1)
+for _ in range(N):
+    a, b = map(int, input().split())
+    if a: garo[b] = 1 # sero line
+    else: sero[b] = 1 # garo line
+garo[W], sero[H] = 1, 1
+maxgaro = find_max(garo, W+1)
+maxsero = find_max(sero, H+1)
+print(maxgaro * maxsero)
