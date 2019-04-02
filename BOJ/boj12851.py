281068e33c6d60c22aaa53cfea5cfd2699381e37
Eunjeong Park <ejisvip@gmail.com>
2019-04-02 13:28:41 +0900

    boj12851: 숨바꼭질 2

--- BOJ/boj12851.py
+++ BOJ/boj12851.py
@@ -2,27 +2,28 @@
 boj12851. 숨바꼭질 2
 - 최소로 도착 가능한 시간
 - 최소로 도착하는 경우의 수
-
 '''
-from sys import stdin
+from collections import deque
 def bfs(v):
-	check[v] = [0, 1] # time, cnt
-	q = [v]
-	while q != []:
-		x = q.pop(0)
+	check[v] = [0, 1] # path, cnt
+	q = deque()
+	q.append(v)
+	while q:
+		x = q.popleft()
 		if x == B:
+			print(check[x][0])
+			print(check[x][1])
+			return
+		for u in (x-1, x+1, 2*x):
+			if u < 0 or u > 100000:
+				continue
+			if not check[u][1]:	# not visited
+				check[u][0] = check[x][0] + 1	# path
+				check[u][1] = check[x][1]		# cnt
+				q.append(u)
+			elif check[u][1] > 0 and check[u][0] == check[x][0] + 1:	# visited
+				check[u][1] += check[x][1]
 
-
-		if 0 <= x-1 < N and not check[x-1][0]:
-			q.append(x-1)
-
-
-		if 0 <= x+1 < N and not check[x+1][0]:
-			q.append(x+1)
-		if 0 <= 2*x < N and not check[2*x][0]:
-			q.append(2*x)
-
-A, B = map(int, stdin.readline().rstrip().split())
-N = 100001
-check = [[N, 0] for _ in range(N)]
+A, B = map(int, input().split())
+check = [[0, 0] for _ in range(100001)]
 bfs(A)
