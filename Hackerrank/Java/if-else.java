a2a893fdb89925e48c471a4576372635269e36ef
Eunjeong Park <ejisvip@gmail.com>
2019-07-24 10:32:02 +0900

    Create Java/

---
+++ Hackerrank/Java/if-else.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+
+        String ans = "";
+        if (N % 2 == 1 || ((N % 2 == 0) && (N >= 6 && N <= 20))) {
+            ans = "Weird";
+        } 
+        else {
+            ans = "Not Weird";
+        }
+
+        System.out.println(ans);
+    }
+}
