c5162f44527b1f9f0fc97cffa4235e552d43beb9
Eunjeong Park <ejolie.dev@gmail.com>
2019-12-26 18:40:28 +0900

    Create Q807 file

---
+++ LeetCode/src/com/ejolie/medium/_807/Solution.java
@@ -0,0 +1,16 @@
+package com.ejolie.medium._807;
+
+/*
+ * 807. Max Increase to Keep City Skyline
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] grid = {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
+        System.out.println(solution.maxIncreaseKeepingSkyline(grid));
+    }
+
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+
+    }
+}
