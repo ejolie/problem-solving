4546577d8d91b42865f15df21ac43a9c5ec710db
ejolie <ejolie.dev@gmail.com>
2020-01-14 23:43:16 +0900

    Add 344. Reverse String (Easy)

---
+++ LeetCode/src/com/ejolie/easy/_344/Solution.java
@@ -0,0 +1,27 @@
+package com.ejolie.easy._344;
+
+/*
+ * 344. Reverse String
+ */
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        solution.reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+
+    public void reverseString(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            swap(s, i, s.length - 1 - i);
+        }
+    }
+
+    private void swap(char[] s, int i, int j) {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+}
