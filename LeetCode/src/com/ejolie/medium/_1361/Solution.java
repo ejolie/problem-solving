bd4c3204f89da6cff1fbfb0cbc5b97a5e5487cc5
ejolie <ejolie.dev@gmail.com>
2020-02-23 19:07:37 +0900

    Add 1361. Validate Binary Tree Nodes (Medium)

---
+++ LeetCode/src/com/ejolie/medium/_1361/Solution.java
@@ -0,0 +1,30 @@
+package com.ejolie.medium._1361;
+
+/*
+ * 1361. Validate Binary Tree Nodes
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int n = 4;
+        int[] leftChild = {1, -1, 3, -1};
+        int[] rightChild = {2, -1, -1, -1};
+        System.out.println(validateBinaryTreeNodes(n, leftChild, rightChild));
+    }
+
+    public static boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+        int[] inDeg = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (leftChild[i] != -1) inDeg[leftChild[i]]++;
+            if (rightChild[i] != -1) inDeg[rightChild[i]]++;
+        }
+
+        int rootCnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (inDeg[i] == 0) rootCnt++;
+            else if (inDeg[i] > 1) return false;
+        }
+        if (rootCnt != 1) return false;
+        return true;
+    }
+
+}
