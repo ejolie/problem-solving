c639c3b9da2d6e647f901f1645fe0658abe6440a
ejolie <ejolie.dev@gmail.com>
2020-04-16 22:59:54 +0900

    [Codility] Arrays - CyclicRotation

---
+++ Codility/lessons/CyclicRotation.js
@@ -0,0 +1,9 @@
+function solution(A, K) {
+    const len = A.length;
+    const arr = new Array(len).fill(0);
+    for (let i = 0; i < len; i++) {
+        const idx = (i + K) % len;
+        arr[idx] = A[i];
+    }
+    return arr;
+}
