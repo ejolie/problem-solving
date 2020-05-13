5d761f016d5b98af421038a5874ca6282a898901
ejolie <ejolie.dev@gmail.com>
2020-05-13 22:19:38 +0900

    Modify TreeNode structure

--- LeetCode/src/com/ejolie/problems/structure/TreeNode.java
+++ LeetCode/src/com/ejolie/problems/structure/TreeNode.java
@@ -5,7 +5,13 @@ public class TreeNode {
     public TreeNode left;
     public TreeNode right;
 
-    public TreeNode(int x) {
-        val = x;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
