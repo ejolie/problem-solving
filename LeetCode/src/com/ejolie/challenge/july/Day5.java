d6cea2f57522b30fa5730aaf6772eaa0558631bb
ejolie <ejolie.dev@gmail.com>
2020-07-07 22:47:24 +0900

    [LeetCode] July Challenge - Day5. Hamming Distance

---
+++ LeetCode/src/com/ejolie/challenge/july/Day5.java
@@ -0,0 +1,25 @@
+package com.ejolie.challenge.july;
+
+/**
+ * Day5. Hamming Distance
+ */
+public class Day5 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 4;
+        System.out.println(hammingDistance(x, y));
+    }
+
+    public static int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int cnt = 0;
+        for (int i = 0; i < 32; i++) {
+            cnt += (xor >> i) & 1;
+        }
+        return cnt;
+    }
+
+    public static int hammingDistance2(int x, int y) {
+        return Integer.bitCount(x ^ y);
+    }
+}
