8292e88f48b11191b5b835b0663fb5c542ea2516
ejolie <ejolie.dev@gmail.com>
2020-07-26 19:20:06 +0900

    [Programmers] 완전탐색 - 소수 찾기

--- Programmers/brute_force/pg_42839.java
+++ Programmers/brute_force/pg_42839.java
@@ -25,7 +25,6 @@ public class pg_42839 {
 
     private static void backtrack(char[] numbers, String num, int r, int depth, boolean[] visited, Set<Integer> set) {
         if (r == depth) {
-            System.out.println(num);
             int intNum = Integer.parseInt(num);
             if (isPrime(intNum)) set.add(intNum);
             return;
@@ -40,7 +39,7 @@ public class pg_42839 {
 
     private static boolean isPrime(int num) {
         if (num <= 1) return false;
-        for (int i = 2; i <= num / 2; i++) {
+        for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) return false;
         }
         return true;
