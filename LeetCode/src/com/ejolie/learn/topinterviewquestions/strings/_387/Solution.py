4b85d40623bcebdf5e716d65011ac191760db1a5
ejolie <ejolie.dev@gmail.com>
2020-07-29 00:23:56 +0900

    [LeetCode] 387. First Unique Character in a String

---
+++ LeetCode/src/com/ejolie/learn/topinterviewquestions/strings/_387/Solution.py
@@ -0,0 +1,22 @@
+'''
+387. First Unique Character in a String
+'''
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        dic = {}
+        for i in range(len(s)):
+            if s[i] not in dic:
+                dic[s[i]] = 1
+            else:
+                dic[s[i]] += 1
+
+        for i in range(len(s)):
+            if dic[s[i]] == 1:
+                return i
+        return -1
+
+
+solution = Solution()
+print(solution.firstUniqChar("leetcode"))
