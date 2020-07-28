5627169c1475021f71159bbcdc161596b766de9a
ejolie <ejolie.dev@gmail.com>
2020-07-28 23:54:29 +0900

    [LeetCode] 344. Reverse String

--- LeetCode/src/com/ejolie/learn/topinterviewquestions/strings/_344/Solution.py
+++ LeetCode/src/com/ejolie/learn/topinterviewquestions/strings/_344/Solution.py
@@ -6,3 +6,13 @@ class Solution:
         n = len(s)
         for i in range(0, n//2):
             s[i], s[-1-i] = s[-1-i], s[i]
+
+    def reverseString_reverse(self, s: List[str]) -> None:
+        s.reverse()
+
+    def reverseString_recursive(self, s: List[str]) -> None:
+        def helper(left, right):
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                helper(left + 1, right - 1)
+        helper(0, len(s) - 1)
