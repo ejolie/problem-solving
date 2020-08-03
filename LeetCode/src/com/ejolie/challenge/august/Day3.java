a20e76bd5a0519eb605a8168368a68a33015d42c
ejolie <ejolie.dev@gmail.com>
2020-08-03 22:15:59 +0900

    [LeetCode] August Challenge - Day3. Valid Palindrome

---
+++ LeetCode/src/com/ejolie/challenge/august/Day3.java
@@ -0,0 +1,55 @@
+package com.ejolie.challenge.august;
+
+/**
+ * Day3. Valid Palindrome
+ */
+public class Day3 {
+    public static void main(String[] args) {
+        String s1 = "A man, a plan, a canal: Panama";
+        String s2 = "race a car";
+        System.out.println(isPalindrome(s1));
+        System.out.println(isPalindrome(s2));
+    }
+
+    public static boolean isPalindrome(String s) {
+        if (s.isEmpty()) return true;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isAlphabetic(s.charAt(i))) {
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            } else if (Character.isDigit(s.charAt(i))) {
+                sb.append(s.charAt(i));
+            }
+        }
+
+        for (int i = 0; i < sb.length() / 2; i++) {
+            if (sb.charAt(i) != sb.charAt(sb.length()-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isPalindrome2(String s) {
+        if (s.isEmpty()) return true;
+        int head = 0;
+        int tail = s.length() - 1;
+        char cHead, cTail;
+        while (head <= tail) {
+            cHead = s.charAt(head);
+            cTail = s.charAt(tail);
+            if (!Character.isLetterOrDigit(cHead)) {
+                head++;
+            } else if (!Character.isLetterOrDigit(cTail)) {
+                tail--;
+            } else {
+                if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+                    return false;
+                }
+                head++;
+                tail--;
+            }
+        }
+        return true;
+    }
+}
