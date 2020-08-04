e967f8c7a3b7717f188cf7aa88ef49b13d5a217a
ejolie <ejolie.dev@gmail.com>
2020-08-04 21:04:54 +0900

    [LeetCode] August Challenge - Day4. Power of Four

---
+++ LeetCode/src/com/ejolie/challenge/august/Day4.java
@@ -0,0 +1,25 @@
+package com.ejolie.challenge.august;
+
+/**
+ * Day4. Power of Four
+ */
+public class Day4 {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfFour(16));
+        System.out.println(isPowerOfFour(5));
+    }
+
+    /**
+     * 4: 100
+     * 3: 011
+     *
+     * 16: 10000
+     * 15: 01111
+     *
+     * 64: 1000000
+     * 63: 0111111
+     */
+    public static boolean isPowerOfFour(int num) {
+        return (num > 0) && ((num & (num - 1)) == 0) && ((num & 0x55555555) != 0);
+    }
+}
