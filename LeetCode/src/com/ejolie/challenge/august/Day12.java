d74abe2d16bdb6e59a709637ca8eb5417b786ada
ejolie <ejolie.dev@gmail.com>
2020-08-12 23:58:17 +0900

    [LeetCode] August Challenge - Day11. Pascal's Triangle II

---
+++ LeetCode/src/com/ejolie/challenge/august/Day12.java
@@ -0,0 +1,36 @@
+package com.ejolie.challenge.august;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Day12. Pascal's Triangle II
+ */
+public class Day12 {
+    public static void main(String[] args) {
+        int rowIndex = 4;
+        List<Integer> result = getRow(rowIndex);
+        System.out.println(result.toString());
+    }
+
+    public static List<Integer> getRow(int rowIndex) {
+        List<int[]> rows = new LinkedList<>();
+        rows.add(new int[]{1});
+        rows.add(new int[]{1, 1});
+        for (int i = 2; i <= rowIndex; i++) {
+            int[] row = new int[i + 1];
+            for (int j = 0; j < i + 1; j++) {
+                if (j == 0 || j == i) row[j] = 1;
+                else row[j] = rows.get(i - 1)[j - 1] + rows.get(i - 1)[j];
+            }
+            rows.add(row);
+        }
+
+        int[] arr = rows.get(rowIndex);
+        List<Integer> answer = new LinkedList<>();
+        for (int n : arr) {
+            answer.add(n);
+        }
+        return answer;
+    }
+}
