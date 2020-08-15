0206689c8e2f7d5d9cb6ac26e12976c7da5ee674
ejolie <ejolie.dev@gmail.com>
2020-08-15 15:37:52 +0900

    [LeetCode] August Challenge - Day14. Longest Palindrome

---
+++ LeetCode/src/com/ejolie/challenge/august/Day14.java
@@ -0,0 +1,26 @@
+package com.ejolie.challenge.august;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Day14. Longest Palindrome
+ */
+public class Day14 {
+    public static void main(String[] args) {
+        String s = "abccccdd";
+        System.out.println(longestPalindrome(s));
+    }
+
+    public static int longestPalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (set.contains(s.charAt(i))) set.remove(s.charAt(i));
+            else set.add(s.charAt(i));
+        }
+
+        int odd = set.size();
+        if (odd == 0) return s.length();
+        return s.length() - odd + 1;
+    }
+}
