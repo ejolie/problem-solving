5070f06482bb2007c0130401ee854dded97d0ae0
ejolie <ejolie.dev@gmail.com>
2020-09-08 01:14:37 +0900

    [Programmers] 2020 KAKAO BLIND RECRUITMENT - 문자열 압축

--- Programmers/challenge/kakao/pg_60057.java
+++ Programmers/challenge/kakao/pg_60057.java
@@ -1,22 +1,25 @@
 package challenge.kakao;
 
+/**
+ * 2020 KAKAO BLIND RECRUITMENT - 문자열 압축
+ */
 public class pg_60057 {
     public static void main(String[] args) {
         String s = "aabbaccc";
         System.out.println(solution(s));
     }
-    
+
     public static int solution(String s) {
         int len = s.length(), lim = len / 2;
         String minString = s;
 
         for (int n = 1; n <= lim; n++) {
-            StringBuffer sb = new StringBuffer();
+            StringBuilder sb = new StringBuilder();
             String prev = s.substring(0, n), next = "";
             int prevCnt = 0, i = 0;
 
-            for (i = 0; i < len-n+1; i += n) {
-                next = s.substring(i, i+n);
+            for (i = 0; i < len - n + 1; i += n) {
+                next = s.substring(i, i + n);
                 if (next.equals(prev)) {
                     prevCnt++;
                 } else {
@@ -42,7 +45,7 @@ public class pg_60057 {
                 minString = sb.toString();
             }
         }
-        
+
         return minString.length();
     }
 }
