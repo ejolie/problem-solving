b5466b3b1389184746deb50933d07b7a6bcae58d
ejolie <ejolie.dev@gmail.com>
2020-09-07 21:28:32 +0900

    [LeetCode] September Challenge - Day7. Word Pattern

---
+++ LeetCode/src/com/ejolie/challenge/september/Day7.java
@@ -0,0 +1,35 @@
+package com.ejolie.challenge.september;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Day7. Word Pattern
+ */
+public class Day7 {
+    public static void main(String[] args) {
+        String pattern = "abba";
+        String str = "dog dog dog dog";
+        System.out.println(wordPattern(pattern, str));
+    }
+
+    public static boolean wordPattern(String pattern, String str) {
+        int N = pattern.length();
+        Map<Character, String> map = new HashMap<>();
+        String[] words = str.split(" ");
+        if (words.length != N) return false;
+
+        for (int i = 0; i < N; i++) {
+            char key = pattern.charAt(i);
+            String value = map.getOrDefault(key, "");
+            if (map.containsKey(key)) {
+                if (!value.equals(words[i])) return false;
+            } else {
+                if (map.containsValue(words[i])) return false;
+                map.put(key, words[i]);
+            }
+        }
+
+        return true;
+    }
+}
