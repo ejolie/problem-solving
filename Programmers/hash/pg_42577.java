29931cc5e673ba858ee4a8b0b1e9abffc28b9615
ejolie <ejolie.dev@gmail.com>
2020-11-22 23:22:10 +0900

    [Programmers] 해시 - 42577. 전화번호 목록

--- Programmers/hash/pg_42577.java
+++ Programmers/hash/pg_42577.java
@@ -1,29 +1,45 @@
 package hash;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * 전화번호 목록
  */
 public class pg_42577 {
     public static void main(String[] args) {
-        String[] phone_book = {
-            "119",
-            "97674223",
-            "1195524421"
-        };
-        System.out.println(solution(phone_book));
+        String[] phone_book = {"123", "456", "789"};
+        System.out.println(solution_hash(phone_book));
     }
 
-    public static boolean solution(String[] phone_book) {
+    public static boolean solution_sort(String[] phone_book) {
         Arrays.sort(phone_book);
-        String prev = phone_book[0];
-        for (int i = 1; i < phone_book.length; i++) {
-            if (phone_book[i].startsWith(prev)) {
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
                 return false;
             }
-            prev = phone_book[i];
         }
+
+        return true;
+    }
+
+    public static boolean solution_hash(String[] phone_book) {
+        Set<String> set = new HashSet<>();
+        for (String phone : phone_book) {
+            set.add(phone);
+        }
+
+        for (String phone : phone_book) {
+            for (int i = 0; i < phone.length() - 1; i++) {
+                String substr = phone.substring(0, i + 1);
+                if (set.contains(substr)) {
+                    return false;
+                }
+            }
+        }
+
         return true;
     }
 }
