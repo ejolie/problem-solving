741b10964e6d3d41dc0a33792b5c6617982b3d04
ejolie <ejolie.dev@gmail.com>
2020-11-30 21:06:03 +0900

    [Programmers] 스택/큐 - 42584. 주식가격

--- Programmers/stack_queue/pg_42584.java
+++ Programmers/stack_queue/pg_42584.java
@@ -11,22 +11,24 @@ public class pg_42584 {
         int[] prices = {1, 2, 3, 2, 3, 3, 1}; // {6, 5, 1, 3, 2, 1, 0}
         System.out.println(Arrays.toString(solution_stack(prices)));
 
-        int[] max_tc = new int[100000]; // {9, 9, 9, 9, 9, 9, 9, ..., 9, 0}
-        for (int i = 0; i < max_tc.length - 1; i++) {
-            max_tc[i] = 9;
+        // {9, 9, 9, 9, 9,..., 9, 0} -> 0일 때는 99999개를 확인 / 여도 O(n)임
+        int[] maxTC = new int[100000];
+        for (int i = 0; i < maxTC.length - 1; i++) {
+            maxTC[i] = 9;
         }
-        System.out.println(solution_stack(max_tc).length);
+        System.out.println(solution_stack(maxTC).length);
         // System.out.println(solution_bruteforce(max_tc).length); // Runtime Error
     }
 
+    // O(n^2)
     public static int[] solution_bruteforce(int[] prices) {
         int N = prices.length;
         int[] answer = new int[N];
 
         for (int i = 0; i < N; i++) {
             for (int j = i + 1; j < N; j++) {
-                answer[i]++;
-                if (prices[i] > prices[j]) {
+                answer[i]++; // 계속 시간은 흐름
+                if (prices[i] > prices[j]) { // 가격이 떨어지는 순간
                     break;
                 }
             }
@@ -35,6 +37,8 @@ public class pg_42584 {
         return answer;
     }
 
+    // O(n)
+    // push, pop - O(1)
     public static int[] solution_stack(int[] prices) {
         int N = prices.length;
         int[] answer = new int[N];
@@ -56,11 +60,8 @@ public class pg_42584 {
         return answer;
     }
 
-    // 1. 가격이 떨어지지 않는 기간 = prices[a]보다 작은 최초의 prices[b]를 발견할 때까지 (a < b, a는 b보다 뒤에 위치)
+    // 1. 가격이 떨어지지 않는 기간 = prices[a]보다 작은 최초의 prices[b]를 발견할 때까지의 기간 (a < b, a는 b보다 앞에 위치)
     // 2. prices[a]보다 작은지 판단하기 위해 value를, 기간을 계산하기 위해 index를 알아야 함 -> 스택에 같이 저장
-    //
-    // prices = {1, 2, 3, 2, 3, 3, 1}
-    // answer = {6, 5, 1, 3, 2, 1, 0}
     public static int[] solution_stack_old(int[] prices) {
         int N = prices.length;
         int[] answer = new int[N];
@@ -68,7 +69,7 @@ public class pg_42584 {
 
         for (int i = 0; i < N; i++) {
             int price = prices[i];
-            if (!stack.empty() && stack.peek()[1] <= price) {
+            if (stack.empty() || stack.peek()[1] <= price) {
                 stack.push(new int[] {i, price});
             } else {
                 // price(prices[b])는 앞에 나온 어떤 prices[a] 보다 작은 최초의 원소다.
@@ -84,7 +85,7 @@ public class pg_42584 {
         // 0 ~ N까지 순회했는데도 계속 가격이 유지된 애들
         while (!stack.empty()) {
             int idx = stack.pop()[0];
-            answer[idx] = N - 1 - idx; // 마지막 인덱스 - a
+            answer[idx] = N - 1 - idx;
         }
 
         return answer;
