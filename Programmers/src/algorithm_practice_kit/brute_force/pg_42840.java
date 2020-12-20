3d11a3cc8e1fca2d374296605fad02b1e711107d
ejolie <ejolie.dev@gmail.com>
2020-12-20 15:15:47 +0900

    [Programmers] 완전탐색 - 모의고사

--- Programmers/src/algorithm_practice_kit/brute_force/pg_42840.java
+++ Programmers/src/algorithm_practice_kit/brute_force/pg_42840.java
@@ -2,16 +2,16 @@ package algorithm_practice_kit.brute_force;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**
  * 모의고사
  */
 public class pg_42840 {
     public static void main(String[] args) {
-        int[] answer = {1, 3, 2, 4, 2};
-        int[] result = solution(answer);
-        System.out.println(Arrays.toString(result));
+        assertArrayEquals(new int[]{1}, solution(new int[]{1, 2, 3, 4, 5}));
+        assertArrayEquals(new int[]{1, 2, 3}, solution(new int[]{1, 3, 2, 4, 2}));
     }
 
     public static int[] solution(int[] answers) {
@@ -23,16 +23,28 @@ public class pg_42840 {
         int cntB = 0;
         int cntC = 0;
         for (int i = 0; i < answers.length; i++) {
-            if (A[i % 5] == answers[i]) cntA++;
-            if (B[i % 8] == answers[i]) cntB++;
-            if (C[i % 10] == answers[i]) cntC++;
+            if (A[i % 5] == answers[i]) {
+                cntA++;
+            }
+            if (B[i % 8] == answers[i]) {
+                cntB++;
+            }
+            if (C[i % 10] == answers[i]) {
+                cntC++;
+            }
         }
 
         int cntMax = Math.max(Math.max(cntA, cntB), cntC);
         List<Integer> list = new ArrayList<>();
-        if (cntA == cntMax) list.add(1);
-        if (cntB == cntMax) list.add(2);
-        if (cntC == cntMax) list.add(3);
+        if (cntA == cntMax) {
+            list.add(1);
+        }
+        if (cntB == cntMax) {
+            list.add(2);
+        }
+        if (cntC == cntMax) {
+            list.add(3);
+        }
 
         return list.stream().mapToInt(i -> i).toArray();
     }
