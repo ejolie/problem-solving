fcd36975094a046ee6ce70816f9dbebe53901ecf
ejolie <ejolie.dev@gmail.com>
2020-12-27 23:44:15 +0900

    Add a solution using BST

--- Algospot/src/greedy/aoj_matchorder.java
+++ Algospot/src/greedy/aoj_matchorder.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 /**
  * 출전 순서 정하기 (난이도: 하)
@@ -35,24 +36,64 @@ public class aoj_matchorder {
             }
 
             int answer = plan();
+//            int answer = plan_bst();
             System.out.println(answer);
         }
     }
 
     public static int plan() {
-        int win = 0;
         Arrays.sort(rusRatings);
         Arrays.sort(korRatings);
 
+        int wins = 0;
         int korOrder = 0, rusOrder = 0;
         while (korOrder < N) {
             if (korRatings[korOrder] >= rusRatings[rusOrder]) {
-                win++;
+                wins++;
                 rusOrder++;
             }
             korOrder++;
         }
 
-        return win;
+        return wins;
     }
+
+    public static int plan_bst() {
+        TreeMap<Integer, Integer> koreans = new TreeMap<>();
+        for (int rating : korRatings) {
+            int count = koreans.getOrDefault(rating, 0);
+            koreans.put(rating, count + 1);
+        }
+
+        int wins = 0;
+        for (int i = 0; i < N; i++) {
+            if (koreans.lastKey() < rusRatings[i]) {
+                // Case 1. 가장 레이팅이 높은 한국 선수가 러시아 선수를 이길 수 없는 경우
+                // 가장 레이팅이 낮은 한국 선수를 내보낸다.
+                int leastRating = koreans.firstKey();
+                int count = koreans.get(leastRating);
+
+                if (count == 1) {
+                    koreans.pollFirstEntry();
+                } else {
+                    koreans.put(leastRating, count - 1);
+                }
+            } else {
+                // Case 2. 그 외의 경우
+                // 러시아 선수보다 레이팅이 높은 한국 선수 중 가장 레이팅이 낮은 한국 선수를 내보낸다.
+                int lowerBoundRating = koreans.ceilingKey(rusRatings[i]);
+                int count = koreans.get(lowerBoundRating);
+
+                if (count == 1) {
+                    koreans.remove(lowerBoundRating);
+                } else {
+                    koreans.put(lowerBoundRating, count - 1);
+                }
+                wins++;
+            }
+        }
+
+        return wins;
+    }
+
 }
