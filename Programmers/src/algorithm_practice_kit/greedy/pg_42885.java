43a85dce35334eee6800f831d7cd3d45a3b91c16
ejolie <ejolie.dev@gmail.com>
2021-01-13 17:31:23 +0900

    [Programmers] 탐욕법 - 구명보트

--- Programmers/src/algorithm_practice_kit/greedy/pg_42885.java
+++ Programmers/src/algorithm_practice_kit/greedy/pg_42885.java
@@ -11,25 +11,19 @@ public class pg_42885 {
     public static void main(String[] args) {
         assertEquals(3, solution(new int[]{70, 50, 80, 50}, 100));
         assertEquals(3, solution(new int[]{70, 80, 50}, 100));
-        assertEquals(2, solution(new int[]{10, 10, 20, 100}, 100));
     }
 
     public static int solution(int[] people, int limit) {
         Arrays.sort(people);
 
         int l = 0, r = people.length - 1;
-        int boats = 0;
-        int weight = 0;
-        while (l <= r) {
-            weight += people[r];
-            while (l <= r && weight + people[l] <= limit) {
-                weight += people[l++];
+        while (l < r) {
+            if (people[r] + people[l] <= limit) {
+                l++;
             }
-            boats++;
-            weight = 0;
             r--;
         }
 
-        return boats;
+        return people.length - l;
     }
 }
