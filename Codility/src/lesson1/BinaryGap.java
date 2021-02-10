11279f22948bff4570afda9752050540d13fda54
ejolie <ejolie.dev@gmail.com>
2021-02-10 20:47:12 +0900

    Change directory structure

---
+++ Codility/src/lesson1/BinaryGap.java
@@ -0,0 +1,18 @@
+class BinaryGap {
+    public int solution(int N) {
+        String binaryString = Integer.toBinaryString(N);
+        int maxLength = 0;
+        int startIndex = -1;
+
+        for (int i = 0; i < binaryString.length(); i++) {
+            if (binaryString.charAt(i) == '1') {
+                if (startIndex != -1) {
+                    maxLength = Math.max(maxLength, i - startIndex - 1);
+                }
+                startIndex = i;
+            }
+        }
+
+        return maxLength;
+    }
+}
