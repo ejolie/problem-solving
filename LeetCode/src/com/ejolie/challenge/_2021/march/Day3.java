aa6a72d8e26af179981a1b6529d2d6350f33c148
ejolie <ejolie.dev@gmail.com>
2021-03-03 22:14:58 +0900

    [LeetCode] Day3. Missing Number - Add another solution using XOR

---
+++ LeetCode/src/com/ejolie/challenge/_2021/march/Day3.java
@@ -0,0 +1,50 @@
+package com.ejolie.challenge._2021.march;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Day3. Missing Number
+ */
+public class Day3 {
+    public static void main(String[] args) {
+        assertEquals(2, missingNumber(new int[]{3, 0, 1}));
+        assertEquals(2, missingNumber(new int[]{0, 1}));
+        assertEquals(8, missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}));
+        assertEquals(1, missingNumber(new int[]{0}));
+    }
+
+    // 0 ms, 39.2 MB
+    // - time: O(n)
+    // - space: O(1)
+    public static int missingNumber(int[] nums) {
+        int N = nums.length;
+        int expectedSum = (N * (N + 1)) / 2;
+        int realSum = 0;
+        for (int num : nums) {
+            realSum += num;
+        }
+        return expectedSum - realSum;
+    }
+
+    // 2 ms, 39.6 MB
+    public static int missingNumber_stream(int[] nums) {
+        int N = nums.length;
+        int expectedSum = (N * (N + 1)) / 2;
+        int realSum = Arrays.stream(nums).sum();
+        return expectedSum - realSum;
+    }
+
+    // 0 ms, 39.2 MB
+    // - time: O(n)
+    // - space: O(1)
+    // a ^ a = 0
+    public static int missingNumber_xor(int[] nums) {
+        int missing = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i ^ nums[i];
+        }
+        return missing;
+    }
+}
