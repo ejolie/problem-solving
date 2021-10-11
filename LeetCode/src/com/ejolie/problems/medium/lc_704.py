d70603595462cc6591012cf20dc102ff9d9bfa8d
Marron Eunjeong Park <ejolie.dev@gmail.com>
2021-10-11 22:40:28 +0900

    [LeetCode] 704. Binary Search

---
+++ LeetCode/src/com/ejolie/problems/medium/lc_704.py
@@ -0,0 +1,23 @@
+
+from typing import List
+
+"""
+704. Binary Search
+
+cf. https://leetcode.com/problems/binary-search/discuss/423162/Binary-Search-101
+"""
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return -1
